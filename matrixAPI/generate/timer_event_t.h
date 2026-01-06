b91a6c917b96c82fdfe6d2e58cf87fd1b4d9281c
t-ijiro <tamitoijiro1125@gmail.com>
2026-01-06 19:14:41 +0900

    timer_event_t.h を更新

--- matrixAPI/generate/timer_event_t.h
+++ matrixAPI/generate/timer_event_t.h
@@ -3,10 +3,9 @@
 
 typedef enum {
     TASK_NONE               = 0,
-    TASK_GEN_SOFTWARE_TIMER = 1 << 0,
-    TASK_DYNAMIC            = 1 << 1,
-    TASK_GRADATION          = 1 << 2,
-    TASK_SCROLL             = 1 << 3
+    TASK_DYNAMIC            = 1 << 0,
+    TASK_GRADATION          = 1 << 1,
+    TASK_SCROLL             = 1 << 2
 } timer_event_t;
 
 #endif /* TIMER_EVENT_T_H */
