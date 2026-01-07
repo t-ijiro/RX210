a4e0775f67a7f77f9b7aa504527daa2868f5e7b6
t-ijiro <tamitoijiro1125@gmail.com>
2026-01-08 05:19:34 +0900

    pwm_loop.c を作成

---
+++ pwm_loop.c
@@ -0,0 +1,24 @@
+void loop()
+{
+    if(t == shrehold)
+    {
+        matrix_write(0, 0, pwm_mode ? pixel_off : c);
+        matrix_flush();
+    }
+    
+    if(t == 0)
+    {
+        t = T_MAX;
+        shrehold++;
+
+        if(shrehold == T_MAX)
+        {
+            matrix_write(0, 0,pwm_mode ? c : pixel_off);
+            matrix_flush();
+            pwm_mode ^= 1;
+            shrehold = 0;
+        }
+    }
+
+    t--;
+}
