267ff8f0287ef7c073b3bb2f316a111ac0005cd6
t-ijiro <tamitoijiro1125@gmail.com>
2026-02-18 22:16:36 +0900

    Reformat rotary.h with consistent indentation

--- gino/rotary.h
+++ gino/rotary.h
@@ -1,11 +1,11 @@
+/* 
+ * e2417105
+ */
+
 #ifndef ROTARY_H
 #define ROTARY_H
 
 #include <stdint.h>
-
-/* 
- * e2417105
- */
 
 typedef struct{
     int16_t count_new;
@@ -19,17 +19,10 @@ typedef enum{
 }rotary_click_t;
 
 void rotary_init(void);
-
 void rotary_clear(rotary_t *r);
-
 rotary_t rotary_get_instance(int16_t val_new, int16_t val_old);
-
 void rotary_record_new(rotary_t *r);
-
 void rotary_record_old(rotary_t *r);
-
 int16_t rotary_calc_delta(rotary_t *r);
-
 rotary_click_t rotary_get_click_dir(int16_t delta);
-
 #endif /* ROTARY_H */
