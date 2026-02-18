630fe270d8203bee73b66f1b133f1122068cfc3c
t-ijiro <tamitoijiro1125@gmail.com>
2026-02-18 15:07:19 +0900

    rotary.h を更新

--- gino/rotary.h
+++ gino/rotary.h
@@ -4,49 +4,8 @@
 #include <stdint.h>
 
 /* 
- * ///////////// サンプルコード/////////////
- *
- * // -----------------------------------
- * // 初期化
- * // -----------------------------------
- *
- * // ハードウェア初期化
- * rotary_init();
- *
- * // rotary構造体インスタンス取得
- * rotary_t rotary = rotary_get_instance(0, 0);
- *
- * // -----------------------------------
- * // メイン処理
- * // -----------------------------------
- *
- * // 現在のカウント数を記録
- * rotary_record_new(&rotary);
- * 
- * // 過去と現在のカウント数の差を計算
- * int16_t delta = rotary_calc_delta(&rotary);
- * 
- * // 計算結果をもとに左右どちらにクリックしたかを取得
- * rotary_click_t dir = rotary_get_click_dir(delta);
- * 
- * // 右方向にクリックした場合の処理
- * if(dir == CLICK_RIGHT)
- * {
- *      // 右回りした際の処理 //
- *      
- *      // 過去のカウント数を記録
- *      rotary_record_old(&rotary);
- * }
- * // 左方向にクリックした場合の処理
- * else if(dir == CLICK_LEFT)
- * {
- *      // 左回りした際の処理 //
- *
- *      // 過去のカウント数を記録
- *      rotary_record_old(&rotary);
- * }
- * 
- */
+ * e2417105
+ /
 
 typedef struct{
     int16_t count_new;
